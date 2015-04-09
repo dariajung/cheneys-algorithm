7558950aa905c4a55425e801039b967dbd154719
Daria Jung <djj2115cu@gmail.com>
2015-04-08 20:07:39 -0400

    whoo skeelton

--- cheney.c
+++ cheney.c
@@ -26,6 +26,17 @@ typedef struct object {
     white - Unvisited, at the end of tracing, considered garbage 
 
     */
+
+    /* Object on the heap is either an int, or a cons cell */
+    union {
+        int value;
+
+        struct {
+            struct heap_object *car;
+            struct heap_object *cdr;
+        };
+    };
+
 } OBJECT;
 
 /* The heap itself, broken into two semi-spaces. */
@@ -42,6 +53,21 @@ typedef struct semispace {
     intptr_t end;
 
 } SEMISPACE;
+
+/* Initializes the heap */
+void init_heap();
+
+/* Creates an object on the heap, does not set value */
+OBJECT * create_heap_object();
+
+OBJECT * create_integer(int value);
+
+OBJECT * create_cons(OBJECT * car, OBJECT * cdr);
+
+/* Creates something of type NIL, ie: when neither 
+integer nor cons cell is created, but memory allocated.
+Similar to C's (void *)0 */
+OBJECT * create_nil();
 
 /* Set forwarding address from from_space to to_space */
 void forward_to(intptr_t address);
